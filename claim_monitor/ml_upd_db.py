f76e728cd493152f58599374633ad7b4709e1a98
DANLCARTON <ericthiberge@orange.fr>
2023-01-12 17:53:11 +0100

    categories

--- claim_monitor/ml_upd_db.py
+++ claim_monitor/ml_upd_db.py
@@ -89,8 +89,43 @@ def testText(text):
     X_new_tfidf = vectorizer.transform(docs_new)
     predicted = clf_logit.predict(X_new_tfidf)
     if (predicted == 0):
-        print ("category : ", "none")
-    print("predicted : ", predicted)
+        print ("category : ", "0 none")
+    elif (predicted == 1):
+        print("category : ", "1 : ice isn't melting")
+    elif (predicted == 2):
+        print("category : ", "2 : we are heading into ice age")
+    elif (predicted == 3):
+        print("category : ", "3 : weather is cold")
+    elif (predicted == 4):
+        print("category : ", "4 : there is a hiatus in warning")
+    elif (predicted == 5):
+        print("category : ", "5 : sea level rise in exaggerated")
+    elif (predicted == 6):
+        print("category : ", "6 : extremes aren't increasing")
+    elif (predicted == 7):
+        print("category : ", "7 : it's natural cycles")
+    elif (predicted == 8):
+        print("category : ", "8 : there is no evidence for greenhouse effect")
+    elif (predicted == 9):
+        print("category : ", "9 : sensitivity is low")
+    elif (predicted == 10):
+        print("category : ", "10 : there is no species impact")
+    elif (predicted == 11):
+        print("category : ", "11 : CO2 is not a pollutant")
+    elif (predicted == 12):
+        print("category : ", "12 : policies are harmful")
+    elif (predicted == 13):
+        print("category : ", "13 : policies are ineffective")
+    elif (predicted == 14):
+        print("category : ", "14 : clean energy won't work")
+    elif (predicted == 15):
+        print("category : ", "15 : we need energy")
+    elif (predicted == 16):
+        print("category : ", "16 : science in unreliable")
+    elif (predicted == 17):
+        print("category : ", "17 : climate movement is unreliable")
+    else:
+        print("an error occured no category has been found")
 
 if __name__ == "__main__":
-    updateDB()
+    updateDB()  
