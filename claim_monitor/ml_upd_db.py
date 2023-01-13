f3e180db9ebbb4f658729a60ec9dd50e153f2325
DANLCARTON <ericthiberge@orange.fr>
2023-01-13 15:39:54 +0100

    change classification numbers

--- claim_monitor/ml_upd_db.py
+++ claim_monitor/ml_upd_db.py
@@ -38,40 +38,40 @@ def testText(text):
     X_new_tfidf = vectorizer.transform(docs_new)
     predicted = clf_logit.predict(X_new_tfidf)
     if (predicted == 0):
-        print ("category : ", "0 → none")
+        print ("category : ", "0_0 → none")
     elif (predicted == 1):
-        print("category : ", "1 → ice isn't melting")
+        print("category : ", "1_1 → ice isn't melting")
     elif (predicted == 2):
-        print("category : ", "2 → we are heading into ice age")
+        print("category : ", "1_2 → we are heading into ice age")
     elif (predicted == 3):
-        print("category : ", "3 → weather is cold")
+        print("category : ", "1_3 → weather is cold")
     elif (predicted == 4):
-        print("category : ", "4 → there is a hiatus in warning")
+        print("category : ", "1_4 → there is a hiatus in warning")
     elif (predicted == 5):
-        print("category : ", "5 → sea level rise is exaggerated")
+        print("category : ", "1_6 → sea level rise is exaggerated")
     elif (predicted == 6):
-        print("category : ", "6 → extremes aren't increasing")
+        print("category : ", "1_7 → extremes aren't increasing")
     elif (predicted == 7):
-        print("category : ", "7 → it's natural cycles")
+        print("category : ", "2_1 → it's natural cycles")
     elif (predicted == 8):
-        print("category : ", "8 → there is no evidence for greenhouse effect")
+        print("category : ", "2_3 → there is no evidence for greenhouse effect")
     elif (predicted == 9):
-        print("category : ", "9 → sensitivity is low")
+        print("category : ", "3_1 → sensitivity is low")
     elif (predicted == 10):
-        print("category : ", "10 → there is no species impact")
+        print("category : ", "3_2 → there is no species impact")
     elif (predicted == 11):
-        print("category : ", "11 → CO2 is not a pollutant")
+        print("category : ", "3_3 → CO2 is not a pollutant")
     elif (predicted == 12):
-        print("category : ", "12 → policies are harmful")
+        print("category : ", "4_1 → policies are harmful")
     elif (predicted == 13):
-        print("category : ", "13 → policies are ineffective")
+        print("category : ", "4_2 → policies are ineffective")
     elif (predicted == 14):
-        print("category : ", "14 → clean energy won't work")
+        print("category : ", "4_4 → clean energy won't work")
     elif (predicted == 15):
-        print("category : ", "15 → we need energy")
+        print("category : ", "4_5 → we need energy")
     elif (predicted == 16):
-        print("category : ", "16 → science in unreliable")
+        print("category : ", "5_1 → science in unreliable")
     elif (predicted == 17):
-        print("category : ", "17 → climate movement is unreliable")
+        print("category : ", "5_2 → climate movement is unreliable")
     else:
         print("an error occured : no category has been found")
